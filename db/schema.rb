cf914b2dc6c92b8cc599c396f158943614177337
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-10 14:19:14 -0400

    working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_10_161354) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_10_174952) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
